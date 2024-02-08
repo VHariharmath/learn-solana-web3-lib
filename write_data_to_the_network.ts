b8ee11568893bb9102c01467ea266e7d39f221ac
root <root@Reno>
2024-02-08 18:11:31 +0530

    Solana

--- write_data_to_the_network.ts
+++ write_data_to_the_network.ts
@@ -8,8 +8,8 @@ import dotenv from "dotenv";
 // loads .env
 dotenv.config();
 
-const keypair_payer = await getKeypairFromEnvironment("SECRET_KEY_PAYER");
-const address = new web3.PublicKey(keypair_payer.publicKey);
+const keypair_sender = await getKeypairFromEnvironment("SECRET_KEY_PAYER");
+const address = new web3.PublicKey(keypair_sender.publicKey);
 
 const connection = new web3.Connection(web3.clusterApiUrl("devnet"));
 var balanceInLamports = await connection.getBalance(address);
@@ -18,7 +18,7 @@ var balanceInSol = balanceInLamports / web3.LAMPORTS_PER_SOL;
 if (balanceInSol < 1) {
   console.log("Adding faucets");
   await connection.requestAirdrop(
-    keypair_payer.publicKey,
+    keypair_sender.publicKey,
     1 * web3.LAMPORTS_PER_SOL
   );
 }
@@ -26,14 +26,12 @@ if (balanceInSol < 1) {
 balanceInLamports = await connection.getBalance(address);
 balanceInSol = balanceInLamports / web3.LAMPORTS_PER_SOL;
 console.log(`SOL = ${balanceInSol}`);
-const keypair_payee = await getKeypairFromEnvironment("SECRET_KEY_PAYEE");
 
-await pingProgram(connection, keypair_payer, keypair_payee);
+await pingProgram(connection, keypair_sender);
 
 async function pingProgram(
   connection: web3.Connection,
-  keypair_payer: web3.Keypair,
-  keypair_payee: web3.Keypair
+  keypair_sender: web3.Keypair
 ) {
   const PING_PROGRAM_ADDRESS = new web3.PublicKey(
     "ChT1B39WKLS8qUrkLvFDXMhEJ4F1XZzwUNHUt4AU9aVa"
@@ -41,10 +39,6 @@ async function pingProgram(
   const PING_PROGRAM_DATA_ADDRESS = new web3.PublicKey(
     "Ah9K7dQ8EHaZqcAsgBW8w37yN2eAy3koFmUn4x3CJtod"
   );
-
-  // const PING_PROGRAM_ADDRESS = new web3.PublicKey(keypair_payer.publicKey);
-  // const PING_PROGRAM_DATA_ADDRESS = new web3.PublicKey(keypair_payee.publicKey);
-  console.log(`PAYEE = ${PING_PROGRAM_DATA_ADDRESS}`);
 
   const transaction = new web3.Transaction();
   const programId = new web3.PublicKey(PING_PROGRAM_ADDRESS);
@@ -66,12 +60,12 @@ async function pingProgram(
   const signature = await web3.sendAndConfirmTransaction(
     connection,
     transaction,
-    [keypair_payer]
+    [keypair_sender]
   );
 
   console.log(`Singnature = ${signature}`);
 
   const balance = await connection.getBalance(programDataId);
   const balanceInSol = balance / web3.LAMPORTS_PER_SOL;
-  console.log(`Transferred ${balanceInSol} to ${programDataId}`);
+  console.log(`Succesfully wrong data to the n/w`);
 }
